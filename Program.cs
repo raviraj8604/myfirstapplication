93102a8a70b45d0e3edbbdf69f39de21a4e258a9
raviraj8604 <117010788+raviraj8604@users.noreply.github.com>
2022-10-30 22:18:19 +0530

    firsd

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This In My First App");
+            int G;
+
+            // Pass variable G to the method
+            // using out keyword
+            Sum(out G);
+
+            // Display the value G
+            Console.WriteLine("The sum of" +
+                    " the value is: {0}", G);
+            Console.ReadLine();
+        }
+
+
+
+        public static void Sum(out int G)
+        {
+            G = 80;
+            G += G;
+        }
+
+
+
+        static void SetValue(ref string str1)
+        {
+
+            // Check parameter value
+            if (str1 == "Geek")
+            {
+                Console.WriteLine("Hello!!Geek");
+            }
+
+            // Assign the new value
+            // of the parameter
+            str1 = "GeeksforGeeks";
+        }
+    }
+    class Program1
+    {
+        //public void Main()
+        //{
+        //    Console.WriteLine("This In My First App");
+        //    Console.ReadLine();
+        //}
+
+
+
+    }
+}
